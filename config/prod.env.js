2bffa86e87d3224ee411609e327884926acfb91e
zhangletian <zhangletian@dtd365.com>
2018-11-08 17:51:44 +0800

    1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '"http://10.51.15.215:8000"',
 }
 // 192.168.0.143   root  密码： P@ssw0rd
